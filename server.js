0d27a65cfdebdd1d56533c6af172deb4c50d41a8
Hasimy <ahmadhasyimi674@gmail.com>
2020-01-14 21:43:55 +0700

    initialize backend

---
+++ server.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+app = express();
+const PORT = 3001 || process.env.PORT;
+
+app
+  .get("/", (req, res) => res.end("halo from serverrr"))
+  .listen(PORT, err => {
+    if (err) throw err;
+    console.log(`Runs in ${PORT}`);
+  });
